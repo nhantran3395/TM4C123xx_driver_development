5f0d3332d68eebed5aa45730f86fd7df79c23aa1
Tran Thanh Nhan <tranthanhnhan3395@gmail.com>
2019-07-28 08:39:39 -0600

    code GPIO pin 's input and output mode related APIs

--- tm4c123xx_gpio.c
+++ tm4c123xx_gpio.c
@@ -9,3 +9,177 @@
 */
 
 #include "tm4c123xx_gpio.h"  
+
+void GPIO_CLK_ctr (GPIOA_Type *GPIOxPtr, uint8_t enOrDis)
+{
+	if (enOrDis == ENABLE){
+		if(GPIOxPtr==GPIOA){
+			SYSCTL->RCGCGPIO |= (1<<0);
+		}else if(GPIOxPtr==GPIOB){
+			SYSCTL->RCGCGPIO |= (1<<1);
+		}else if(GPIOxPtr==GPIOC){
+			SYSCTL->RCGCGPIO |= (1<<2);
+		}else if(GPIOxPtr==GPIOD){
+			SYSCTL->RCGCGPIO |= (1<<3);
+		}else if(GPIOxPtr==GPIOE){
+			SYSCTL->RCGCGPIO |= (1<<4);
+		}else if(GPIOxPtr==GPIOF){
+			SYSCTL->RCGCGPIO |= (1<<5);
+		}			
+	}else{
+		if(GPIOxPtr==GPIOA){
+			SYSCTL->RCGCGPIO &= ~(1<<0);
+		}else if(GPIOxPtr==GPIOB){
+			SYSCTL->RCGCGPIO &= ~(1<<1);
+		}else if(GPIOxPtr==GPIOC){
+			SYSCTL->RCGCGPIO &= ~(1<<2);
+		}else if(GPIOxPtr==GPIOD){
+			SYSCTL->RCGCGPIO &= ~(1<<3);
+		}else if(GPIOxPtr==GPIOE){
+			SYSCTL->RCGCGPIO &= ~(1<<4);
+		}else if(GPIOxPtr==GPIOF){
+			SYSCTL->RCGCGPIO &= ~(1<<5);
+		}
+	}
+}
+
+void GPIO_init (GPIO_Handle_t *GPIOpinxHandlePtr){
+	uint8_t pinMode = GPIOpinxHandlePtr->GPIOpinConfigPtr->mode;
+	uint8_t pinNumber = GPIOpinxHandlePtr->GPIOpinConfigPtr->pinNumber;
+	uint8_t puPdr = GPIOpinxHandlePtr->GPIOpinConfigPtr->puPdr;
+	uint8_t outType = GPIOpinxHandlePtr->GPIOpinConfigPtr->outType;
+	uint8_t drive = GPIOpinxHandlePtr->GPIOpinConfigPtr->driveSel;
+	uint8_t slewRate = GPIOpinxHandlePtr->GPIOpinConfigPtr->slewRateCtrl;
+	
+	if(pinMode == GPIO_MODE_IN){	
+		/*configuration for GPIO input pin*/
+		GPIOpinxHandlePtr->GPIOxPtr->AFSEL &= ~(1<<pinNumber);
+		GPIOpinxHandlePtr->GPIOxPtr->DIR &= ~(1<<pinNumber);
+		GPIOpinxHandlePtr->GPIOxPtr->ODR &= ~(1<<pinNumber);
+		GPIOpinxHandlePtr->GPIOxPtr->DEN |= 1<<pinNumber;
+		
+		if(puPdr == GPIO_NO_PUPDR){
+			GPIOpinxHandlePtr->GPIOxPtr->PUR &= ~(1<<pinNumber);
+			GPIOpinxHandlePtr->GPIOxPtr->PDR &= ~(1<<pinNumber);
+		}else if(puPdr == GPIO_PU){
+			GPIOpinxHandlePtr->GPIOxPtr->PUR |= 1<<pinNumber;
+		}else if(puPdr == GPIO_PDR){
+			GPIOpinxHandlePtr->GPIOxPtr->PDR |= 1<<pinNumber;
+		}
+	}else if(pinMode == GPIO_MODE_OUT){
+		
+		/*configuration for GPIO output pin*/
+		GPIOpinxHandlePtr->GPIOxPtr->AFSEL &= ~(1<<pinNumber);
+		GPIOpinxHandlePtr->GPIOxPtr->DIR |= 1<<pinNumber;
+		GPIOpinxHandlePtr->GPIOxPtr->DEN |= 1<<pinNumber;
+		
+		if(outType == GPIO_OTYPE_OD){
+			/*open drain mode*/
+			GPIOpinxHandlePtr->GPIOxPtr->ODR |= 1<<pinNumber;
+		}else if(outType == GPIO_OTYPE_NONE){
+			/*none open drain mode; pull up or pull down or none*/
+			GPIOpinxHandlePtr->GPIOxPtr->ODR &= ~(1<<pinNumber);
+			if(puPdr == GPIO_NO_PUPDR){
+				GPIOpinxHandlePtr->GPIOxPtr->PUR &= ~(1<<pinNumber);
+				GPIOpinxHandlePtr->GPIOxPtr->PDR &= ~(1<<pinNumber);
+			}else if(puPdr == GPIO_PU){
+				GPIOpinxHandlePtr->GPIOxPtr->PUR |= 1<<pinNumber;
+			}else if(puPdr == GPIO_PDR){
+				GPIOpinxHandlePtr->GPIOxPtr->PDR |= 1<<pinNumber;
+			}
+		}
+		
+		if(drive==GPIO_DRIVE_2MA){
+			GPIOpinxHandlePtr->GPIOxPtr->DR2R |= 1<<pinNumber;
+		}else if (drive==GPIO_DRIVE_4MA){
+			GPIOpinxHandlePtr->GPIOxPtr->DR2R |= 1<<pinNumber;
+		}else if (drive==GPIO_DRIVE_8MA){
+			GPIOpinxHandlePtr->GPIOxPtr->DR8R |= 1<<pinNumber;
+			if(slewRate == GPIO_SLEWRATE_EN){
+					GPIOpinxHandlePtr->GPIOxPtr->SLR |= 1<<pinNumber;
+			}else{
+					GPIOpinxHandlePtr->GPIOxPtr->SLR &= ~(1<<pinNumber);
+			}
+		}		
+	}
+}
+
+/**
+*@brief Deinitilize GPIO pin
+*@param Pointer to base address of GPIO port x registers
+*@return none
+*/
+void GPIO_deinit (GPIOA_Type *GPIOxPtr);
+
+/**
+*@brief Read data from GPIO input pin
+*@param Pointer to base address of GPIO port x registers
+*@param GPIO pin number
+*@return Data from GPIO input pin
+*/
+uint8_t GPIO_read_pin (GPIOA_Type *GPIOxPtr, uint8_t pinNumber)
+{
+	return (uint8_t)(GPIOxPtr->DATA>>pinNumber)&0x01;
+}
+
+/**
+*@brief Read data from GPIO input port
+*@param Pointer to base address of GPIO port x registers
+*@return Data from GPIO port
+*/
+uint16_t GPIO_read_port (GPIOA_Type *GPIOxPtr);
+
+/**
+*@brief Write data to GPIO output pin
+*@param Pointer to base address of GPIO port x registers
+*@param GPIO pin number
+*@param Set or clear action
+*@return none
+*/
+void GPIO_write_pin (GPIOA_Type *GPIOxPtr, uint8_t pinNumber, uint8_t setOrClear)
+{
+	if(setOrClear == SET){
+		GPIOxPtr->DATA = 1<<pinNumber;
+	}else{
+		GPIOxPtr->DATA = ~(1<<pinNumber);
+	}
+}
+
+/**
+*@brief Write data to GPIO output port
+*@param Pointer to base address of GPIO port x registers
+*@param Set or clear action
+*@return none
+*/
+void GPIO_write_port (GPIOA_Type *GPIOxPtr);
+
+/**
+*@brief Toggle GPIO output pin
+*@param Pointer to base address of GPIO port x registers
+*@param GPIO pin number
+*@return none
+*/
+void GPIO_toggle_pin (GPIOA_Type *GPIOxPtr, uint8_t pinNumber);
+
+/**
+*@brief Config interrupt priority
+*@param IRQ number
+*@param Priority
+*@return none
+*/
+void GPIO_Intrpt_priority_config (uint8_t IRQnumber, uint8_t priority);
+
+/**
+*@brief Enable or disable GPIO pin 's interrupt 
+*@param IRQ number
+*@param Enable or disable action
+*@return none
+*/
+void GPIO_Intrpt_ctrl (uint8_t IRQnumber, uint8_t enOrDis);
+
+/**
+*@brief Handler for GPIO pin 's interrupt
+*@param GPIO pin number
+*@return none
+*/
+void GPIO_Intrpt_handler (uint8_t pinNumber);
--- tm4c123xx_gpio.h
+++ tm4c123xx_gpio.h
@@ -11,7 +11,7 @@
 #ifndef TM4C123XX_GPIO_H
 #define TM4C123XX_GPIO_H
 
-#include "TM4C123.h"                    // Device header
+#include "TM4C123GH6PM.h"                    // Device header
 #include <stdint.h>
 
 #define ENABLE 1
@@ -33,7 +33,7 @@
 #define GPIO_PIN_NO_7	7
 
 /*
-*@GPIO_MODE
+*@GPIO_MODE_SEL
 *GPIO pin possible modes
 */
 #define GPIO_MODE_IN 0
@@ -45,19 +45,51 @@
 #define GPIO_MODE_INTRPT_RFE	6	//Interrupt, both edge
 
 /*
-*GPIO_PUPDR_MODE
-*GPIO pin input modes
+*@GPIO_PUPDR_SEL
+*GPIO pin pull up/ pull down selection
 */
 #define GPIO_NO_PUPDR 0
 #define GPIO_PU 1
 #define GPIO_PDR 2
 
+/*
+*@GPIO_OUTTYPE_SEL
+*GPIO pin open drain selection
+*/
+#define GPIO_OTYPE_NONE 0
+#define GPIO_OTYPE_OD 1
+
+/*
+*@GPIO_DRIVE_SEL
+*GPIO drive current selection
+*/
+#define GPIO_DRIVE_2MA 0
+#define GPIO_DRIVE_4MA 1
+#define GPIO_DRIVE_8MA 2
+
+/*
+*@GPIO_SLEWRATE_SEL
+*GPIO slewrate selection
+*/
+#define GPIO_SLEWRATE_DIS 0
+#define GPIO_SLEWRATE_EN 1
+
+
 typedef struct
 {
+	uint8_t pinNumber;	/*Refer to @GPIO_PIN_NO for possible value*/
+	uint8_t mode;	/*Refer to @GPIO_MODE_SEL for possible value*/
+	uint8_t puPdr;	/*Refer to @GPIO_PUPDR_SEL for possible value*/
+	uint8_t outType;	/*Refer to @GPIO_OUTTYPE_SEL for possible value*/
+	uint8_t driveSel;	/*Refer to @GPIO_DRIVE_SEL for possible value*/
+	uint8_t slewRateCtrl;	/*Refer to @GPIO_SLEWRATE_SEL for possible value*/
+	uint8_t altFunc;	
 }GPIO_Pin_config_t;
 
 typedef struct 
 {
+	GPIOA_Type *GPIOxPtr;
+	GPIO_Pin_config_t *GPIOpinConfigPtr;
 }GPIO_Handle_t;
 
 /**
@@ -66,21 +98,21 @@ typedef struct
 *@param Enable or disable action
 *@return none
 */
-void GPIO_CLK_ctr (void);
+void GPIO_CLK_ctr (GPIOA_Type *GPIOxPtr, uint8_t enOrDis);
 
 /**
 *@brief Initialize GPIO pin
 *@param Pointer to GPIO_Handle struct
 *@return none
 */
-void GPIO_init (void);
+void GPIO_init (GPIO_Handle_t *GPIOpinxHandlePtr);
 
 /**
 *@brief Deinitilize GPIO pin
 *@param Pointer to base address of GPIO port x registers
 *@return none
 */
-void GPIO_deinit (void);
+void GPIO_deinit (GPIOA_Type *GPIOxPtr);
 
 /**
 *@brief Read data from GPIO input pin
@@ -88,14 +120,14 @@ void GPIO_deinit (void);
 *@param GPIO pin number
 *@return Data from GPIO input pin
 */
-uint8_t GPIO_read_pin (void);
+uint8_t GPIO_read_pin (GPIOA_Type *GPIOxPtr, uint8_t pinNumber);
 
 /**
 *@brief Read data from GPIO input port
 *@param Pointer to base address of GPIO port x registers
 *@return Data from GPIO port
 */
-uint16_t GPIO_read_port (void);
+uint16_t GPIO_read_port (GPIOA_Type *GPIOxPtr);
 
 /**
 *@brief Write data to GPIO output pin
@@ -104,7 +136,7 @@ uint16_t GPIO_read_port (void);
 *@param Set or clear action
 *@return none
 */
-void GPIO_write_pin (void);
+void GPIO_write_pin (GPIOA_Type *GPIOxPtr, uint8_t pinNumber, uint8_t setOrClear);
 
 /**
 *@brief Write data to GPIO output port
@@ -112,7 +144,7 @@ void GPIO_write_pin (void);
 *@param Set or clear action
 *@return none
 */
-void GPIO_write_port (void);
+void GPIO_write_port (GPIOA_Type *GPIOxPtr);
 
 /**
 *@brief Toggle GPIO output pin
@@ -120,7 +152,7 @@ void GPIO_write_port (void);
 *@param GPIO pin number
 *@return none
 */
-void GPIO_toggle_pin (void);
+void GPIO_toggle_pin (GPIOA_Type *GPIOxPtr, uint8_t pinNumber);
 
 /**
 *@brief Config interrupt priority
@@ -128,7 +160,7 @@ void GPIO_toggle_pin (void);
 *@param Priority
 *@return none
 */
-void GPIO_Intrpt_priority_config (void);
+void GPIO_Intrpt_priority_config (uint8_t IRQnumber, uint8_t priority);
 
 /**
 *@brief Enable or disable GPIO pin 's interrupt 
@@ -136,13 +168,13 @@ void GPIO_Intrpt_priority_config (void);
 *@param Enable or disable action
 *@return none
 */
-void GPIO_Intrpt_ctrl (void);
+void GPIO_Intrpt_ctrl (uint8_t IRQnumber, uint8_t enOrDis);
 
 /**
 *@brief Handler for GPIO pin 's interrupt
 *@param GPIO pin number
 *@return none
 */
-void GPIO_Intrpt_handler (void);
+void GPIO_Intrpt_handler (uint8_t pinNumber);
 
 #endif 
